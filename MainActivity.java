ee56609f8a1aa510e4c63c9798b3ab2bbcfa8b5b
Aakash Jha <72294865+sky-jha@users.noreply.github.com>
2022-10-15 20:44:53 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,42 @@
+public class MainActivity extends AppCompatActivity {
+
+    private TextView userView;
+    private TextView songView;
+    private Button addBtn;
+    private Song song;
+
+    private SongService songService;
+    private ArrayList<Song> recentlyPlayedTracks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        songService = new SongService(getApplicationContext());
+        userView = (TextView) findViewById(R.id.user);
+        songView = (TextView) findViewById(R.id.song);
+        addBtn = (Button) findViewById(R.id.add);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
+        userView.setText(sharedPreferences.getString("userid", "No User"));
+
+        getTracks();
+    }
+
+
+     private void getTracks() {
+        songService.getRecentlyPlayedTracks(() -> {
+            recentlyPlayedTracks = songService.getSongs();
+            updateSong();
+        });
+    }
+
+    private void updateSong() {
+        if (recentlyPlayedTracks.size() > 0) {
+            songView.setText(recentlyPlayedTracks.get(0).getName());
+            song = recentlyPlayedTracks.get(0);
+        }
+    }
+
+}
